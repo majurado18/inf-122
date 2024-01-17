6e5b0d3354b3f51e2632ec2863fcc68c49128c66
majurado18 <majuradoveizan@gmail.com>
2024-01-17 06:41:45 -0400

    tres en raya

---
+++ semana3/tres-en-raya-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Tablero from './Componentes/Tablero';
+import Juego from './Componentes/Juego';
+
+function App() {
+  return (
+    <div className="App">
+      <Tablero/>
+      <Juego/>
+    </div>
+  );
+}
+
+export default App;
